8be674780c0378dba2e879da417759ce79dbdc7b
nion068 <mu.islam34@gmail.com>
2019-07-17 02:15:44 +0600

    create_team models missing field problem solved

--- create_team/models.py
+++ create_team/models.py
@@ -3,27 +3,27 @@ from user_registration.models import Players, Users
 
 
 class SeriesList(models.Model):
-    Series_name = models.CharField(max_length=250, unique=True, primary_key=True),
-    No_of_matches = models.IntegerField(),
-    Home_team = models.CharField(max_length=250),
-    Away_team = models.CharField(max_length=250),
+    Series_name = models.CharField(max_length=250, unique=True, primary_key=True)
+    No_of_matches = models.IntegerField()
+    Home_team = models.CharField(max_length=250)
+    Away_team = models.CharField(max_length=250)
 
 
 class SeriesSquads(models.Model):
-    Series_name = models.ForeignKey(SeriesList, on_delete=models.CASCADE),
+    Series_name = models.ForeignKey(SeriesList, on_delete=models.CASCADE)
     Squad_player = models.ForeignKey(Players, on_delete=models.CASCADE)
 
 
 class TeamCreated(models.Model):
-    Series_name = models.ForeignKey(SeriesList, on_delete=models.CASCADE),
-    Match_no = models.IntegerField(),
-    User_id = models.ForeignKey(Users, on_delete=models.CASCADE),
-    Match_day = models.DateField(),
+    Series_name = models.ForeignKey(SeriesList, on_delete=models.CASCADE)
+    Match_no = models.IntegerField()
+    User_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    Match_day = models.DateField()
 
 
 class TeamPlayers(models.Model):
-    Team_created = models.ForeignKey(TeamCreated,on_delete=models.CASCADE),
-    Players = models.ForeignKey(TeamCreated, on_delete=models.CASCADE),
+    Team_created = models.ForeignKey(TeamCreated,on_delete=models.CASCADE)
+    Players = models.ForeignKey(Players, on_delete=models.CASCADE)
 
 
 
